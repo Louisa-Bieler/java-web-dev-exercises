b1798be099af9a0a43e92f2dc5cf96eec0626704
Louisa Lynne Bieler <louisa.lynne.bieler@gmail.com>
2023-01-11 17:46:00 +0100

    work up until 11 January, 2023

--- src/org/launchcode/java/demos/lsn3classes1/Courses.java
+++ src/org/launchcode/java/demos/lsn3classes1/Courses.java
@@ -1,4 +1,58 @@
 package org.launchcode.java.demos.lsn3classes1;
 
+import java.time.LocalDate;
+
 public class Courses {
+    private String instructor;
+    private String department;
+    private final int courseNumber;
+    private int credits;
+    private int capacity;
+    private String semester;
+    private LocalDate startDate;
+
+    //constructor
+
+    public Courses(String instructor, String department, int courseNumber, int credits, int aCapacity, String aSemester, LocalDate aStartDate){
+        this.instructor = instructor;
+        this.department = department;
+        this.courseNumber = courseNumber;
+        this.credits = credits;
+        this.capacity = aCapacity;
+        this.semester = aSemester;
+        this.startDate = aStartDate;
+    }
+
+    public int getCourseNumber(){
+        return courseNumber;
+    }
+
+    public String getSemester(){
+        return semester;
+    }
+
+    public String getInstructor(){
+        return instructor;
+    }
+
+    public String getDepartment(){
+        return department;
+    }
+    public int getCredits(){
+        return credits;
+    }
+
+    public int getCapacity(){
+        return capacity;
+    }
+
+    public LocalDate getStartDate(){
+        return startDate;
+    }
+
+    public void setInstructor(String instructor){
+        this.instructor = instructor
+
+    }
+
 }
